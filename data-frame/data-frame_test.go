29c9f1dfbacc6e0bb7615670a7090e4830d71375
Alex Sanchez <kniren@gmail.com>
2016-08-07 20:18:11 +0200

    Clean df.ReadMaps test code

--- data-frame/data-frame_test.go
+++ data-frame/data-frame_test.go
@@ -321,23 +321,22 @@ func TestDataFrame_Filter(t *testing.T) {
 }
 
 func TestDataFrame_ReadMaps(t *testing.T) {
-	var m []map[string]interface{}
-	one := make(map[string]interface{})
-	one["Age"] = 23
-	one["Name"] = "Alice"
-	one["Credit"] = 12.10
-	two := make(map[string]interface{})
-	two["Age"] = 32
-	two["Name"] = "Bob"
-	two["Credit"] = 15.1
-	three := make(map[string]interface{})
-	three["Age"] = 41
-	three["Name"] = "Daniel"
-	three["Credit"] = 16.2
-	m = []map[string]interface{}{
-		one,
-		two,
-		three,
+	m := []map[string]interface{}{
+		map[string]interface{}{
+			"Age":    23,
+			"Name":   "Alice",
+			"Credit": 12.10,
+		},
+		map[string]interface{}{
+			"Age":    32,
+			"Name":   "Bob",
+			"Credit": 15.1,
+		},
+		map[string]interface{}{
+			"Age":    41,
+			"Name":   "Daniel",
+			"Credit": 16.2,
+		},
 	}
 	b := ReadMaps(m)
 	if b.Err() != nil {
