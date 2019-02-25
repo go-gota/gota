34cbc95886d072ae7deea9caf4ef1db911acf4d3
pa-m <pascal.masschelier@gmail.com>
2019-02-25 19:51:53 +0100

    add support for float32

---
+++ series/math32.go
@@ -0,0 +1,18 @@
+package series
+
+import "math"
+
+type math32T struct{}
+
+// pseudo math32 package. use github.com/chewxy/math32 if you need more
+var math32 math32T
+
+func (math32T) NaN() float32         { return float32(math.NaN()) }
+func (math32T) IsNaN(f float32) bool { return math.IsNaN(float64(f)) }
+func (math32T) IsInf(f float32, sign int) bool {
+	if sign < 0 {
+		return f < -math.MaxFloat32
+	} else {
+		return f > math.MaxFloat32
+	}
+}
