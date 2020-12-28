f164964d25d41d7942254e19fc310a7bfe0ba707
Michał Brzozowski <michal.brzozowski@mim-solutions.pl>
2020-12-29 00:08:47 +0100

    changing float to string precision

--- series/type-float.go
+++ series/type-float.go
@@ -74,7 +74,7 @@ func (e floatElement) String() string {
 	if e.IsNA() {
 		return "NaN"
 	}
-	return fmt.Sprintf("%f", e.e)
+	return fmt.Sprintf("%g", e.e)
 }
 
 func (e floatElement) Int() (int, error) {
